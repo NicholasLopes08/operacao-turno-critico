a79c4c631f897e191262f21a8560a8b472a34592
Nicholas lopes <nicholas.cl@puccampinas.edu.br>
2026-05-28 23:39:21 -0300

    Update auxiliares.py

--- auxiliares.py
+++ auxiliares.py
@@ -1,4 +1,15 @@
-validar_id_pedido()
-buscar_pedido()
-buscar_entregador()
-contar_pedidos_ativos()
+def validar_id_pedido(id_pedido):
+    if len(id_pedido) != 5:
+        return False
+    if id_pedido[0].isalpha() == False:
+        return False
+    if id_pedido[1].isdigit() == False:
+        return False
+    if id_pedido[2].isdigit() == False:
+        return False
+    if id_pedido[3].isdigit() == False:
+        return False
+    if id_pedido[4].isdigit() == False:
+        return False
+    return True
+
